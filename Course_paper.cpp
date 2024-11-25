4be2a3cd4f6d82f61897b5da5863fda7289d92ad
Radik Zabirov <zabirovradik070@gmail.com>
2024-11-25 22:54:59 +0400

    S0 and S1 complete

--- Course_paper.cpp
+++ Course_paper.cpp
@@ -113,7 +113,7 @@ void constructVCS(size_t n, size_t k, cv::Mat image) {
     for (size_t j = 0; j <= r; ++j) {
         c[j] = binomial(n - r - 1 - j, r - j);
     }
-    //std::deque<size_t> is0, is1; // для проверки
+    std::deque<size_t> is0, is1; // для проверки
     // Построение матриц S^0 и S^1
     std::deque < std::vector<size_t>> S0, S1;
     if (k % 2) {
@@ -123,25 +123,55 @@ void constructVCS(size_t n, size_t k, cv::Mat image) {
                     j = n - r;
                 S0.push_back(createBooleanMatrix(n, j, c[j > r ? n - j : j]));
                 S1.push_back(createBooleanMatrix(n, j + 1, c[j > r ? n - j - 1 : j + 1]));
-                //is0.push_back(j > r ? n - j : j);
-                //is1.push_back(j > r ? n - j - 1 : j + 1);
+                is0.push_back(j > r ? n - j : j);
+                is1.push_back(j > r ? n - j - 1 : j + 1);
             }
         else 
             for (int j = 0; j < n; j += 2) {
                 S0.push_back(createBooleanMatrix(n, j, c[j > r ? n - j : j]));
-                //is0.push_back(j > r ? n - j : j);
+                is0.push_back(j);
                 if (j == r)
                     j = n - r - 1;
                 S1.push_back(createBooleanMatrix(n, j + 1, c[j > r ? n - j - 1 : j + 1]));
-                //is1.push_back(j > r ? n - j - 1 : j + 1);
+                is1.push_back(j + 1);
             }
     }
+    else {
+        if (r % 2) {
+            for (int j = 0; j < n; j += 2) {
+                if (j == r + 1)
+                    j = n - r + 1;
+                S0.push_back(createBooleanMatrix(n, j, c[j > r ? n - j + 1 : j]));
+                S1.push_back(createBooleanMatrix(n, j + 1, c[j > r ? n - j: j + 1]));
+                //is0.push_back(j > r ? n - j + 1: j);
+                //is1.push_back(j > r ? n - j : j + 1);
+                is0.push_back(j);
+                is1.push_back(j + 1);
+            }
+            S0.push_back(createBooleanMatrix(n, n, c[1]));
+            is0.push_back(1);
+        }
+        else {
+            for (int j = 0; j < n; j += 2) {
+                S0.push_back(createBooleanMatrix(n, j, c[j > r ? n - j + 1 : j]));
+                is0.push_back(j > r ? n - j + 1 : j);
+                //is0.push_back(j);
+                if (j == r)
+                    j = n - r;
+                S1.push_back(createBooleanMatrix(n, j + 1, c[j > r ? n - j : j + 1]));
+                is1.push_back(j > r ? n - j : j + 1);
+                //is1.push_back(j + 1);
+            }
+            S0.push_back(createBooleanMatrix(n, n, c[1]));
+            is0.push_back(1);
+        }
+    }
 
-    /*for(auto lo: is0)
+    for(auto lo: is0)
         std::cout << lo << " ";
     std::cout << std::endl;
     for (auto lo : is1)
-        std::cout << lo << " ";*/
+        std::cout << lo << " ";
     encryptImage(image, S0, S1, n, k);
 }
 
@@ -150,7 +180,7 @@ void constructVCS(size_t n, size_t k, cv::Mat image) {
 
 int main(){
     size_t n = 17;
-    size_t k = 13;
+    size_t k = 12;
     size_t r = k / 2;
     constructVCS(n, k, visual_cypher("D:\\Course_paper\\table.png"));
     return 0;
